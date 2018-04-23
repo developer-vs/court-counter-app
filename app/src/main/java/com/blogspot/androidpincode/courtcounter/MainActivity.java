17b764e70b0befa6eda37641c9a720bd6b9f284b
webdev-vs <vladimirgdsr@gmail.com>
2018-04-22 23:37:25 -0700

    added three onClick methods into xml and tree Java methods

--- app/src/main/java/com/blogspot/androidpincode/courtcounter/MainActivity.java
+++ app/src/main/java/com/blogspot/androidpincode/courtcounter/MainActivity.java
@@ -2,6 +2,8 @@ package com.blogspot.androidpincode.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Increase the score for Team A by 3 points.
+     */
+    public void addThreePointsForTeamA(View view) {
+        displayForTeamA(3);
+    }
+
+    /**
+     * Increase the score for Team A by 2 points.
+     */
+    public void addTwoPointsForTeamA(View view) {
+        displayForTeamA(2);
+    }
+
+    /**
+     * Increase the score for Team A by 1 point.
+     */
+    public void addOnePointForTeamA(View view) {
+        displayForTeamA(1);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    private void displayForTeamA(int score) {
+        TextView scoreView = findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
     }
 }
